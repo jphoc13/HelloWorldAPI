0a479ec3d6fa37aaf38e94f8dbb40b1a020854a0
JP Hochbaum <jphoc13@gmail.com>
2017-08-16 20:37:22 -0500

    first commit

---
+++ HelloWorldAPITest/HelloWorldInterfaceAPITest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+
+
+namespace HelloWorldAPI.Test
+{
+    [TestClass]
+    public class HelloWorldInterfaceAPIShould
+    {
+        [TestMethod]
+        public void ProperlyCreateConsoleBehaviorAndSetMessage()
+        {
+            //Arrange
+            var app = new App();
+
+            //Act
+            app.setBehavior(new ConsoleBehavior());
+            app.setMessage("Hello World");
+
+            //Assert
+            Assert.IsInstanceOfType(app.getBehavior(), typeof(ConsoleBehavior));
+            Assert.AreEqual("Hello World", app.getMessage());
+        }
+
+        [TestMethod]
+        public void ProperlyCreateDatabaseBehaviorAndSetMessage()
+        {
+            //Arrange
+            var app = new App();
+
+            //Act
+            app.setBehavior(new DatabaseBehavior());
+            app.setMessage("Hello World");
+
+            //Assert
+            Assert.IsInstanceOfType(app.getBehavior(), typeof(DatabaseBehavior));
+            Assert.AreEqual("Hello World", app.getMessage());
+        }
+    }
+}
